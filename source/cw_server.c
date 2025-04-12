28962b9962005b5bc1b4732524751e6d96321095
amurph <aidanpmurphy@hotmail.com>
2025-04-12 15:22:41 -0700

    response buffer is malloc'd and LARGE

--- source/cw_server.c
+++ source/cw_server.c
@@ -11,6 +11,7 @@
 #include "cw_server.h"
 
 #define REQUEST_BUFFER_LEN 4096
+#define RESPONSE_BUFFER_LEN 5242880
 
 int _validate_request() {
 	//TODO
@@ -25,19 +26,19 @@ char* _extract_resource(char* request_string) {
 }
 
 int cw_handle_request(int sockfd, struct sockaddr_in sock_addr, socklen_t sock_addr_len) {
-	uint8_t return_buff[4096+1];
+	char *return_buff = (char*)malloc(RESPONSE_BUFFER_LEN*sizeof(char));
 	ssize_t num_bytes_received;
 	char request_buffer[REQUEST_BUFFER_LEN] = {0};
 
 	num_bytes_received = recv(sockfd, request_buffer, REQUEST_BUFFER_LEN, 0);
 	if (num_bytes_received < 0) {
 		perror("couldn't receive bytes from client\n");
-		return -1;
+		goto cleanup_cw_handle_request;
 	}
 	if (_validate_request() < 0) {
 		printf("received bad request, exiting\n");
 		//TODO: give the user some sort of response
-		return -1;
+		goto cleanup_cw_handle_request;
 	}
 	printf("request: %s\n", request_buffer);
 
@@ -52,12 +53,15 @@ int cw_handle_request(int sockfd, struct sockaddr_in sock_addr, socklen_t sock_a
 	printf("addr length: %d\n", sock_addr_len);
 	
 	// assemble and return response
-	snprintf((char*)return_buff, sizeof(return_buff), "HTTP/1.0 200 OK\r\n\r\nHello");
-	if (write(sockfd, (char*)return_buff, strlen((char*)return_buff)) < 0) {
+	snprintf(return_buff, RESPONSE_BUFFER_LEN*sizeof(char), "HTTP/1.0 200 OK\r\n\r\nHello");
+	if (write(sockfd, return_buff, strlen(return_buff)) < 0) {
 		perror("failed to communicate");
 		printf("failed to write to %s", ip_addr);
 		return 1;
 	}
+
+cleanup_cw_handle_request:
+	free(return_buff);
 
 	return 0;
 }
